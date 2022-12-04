abc71f6b66e583746d51ac8fce58ce4ef0f174f8
angelollima <angeloantonio442@gmail.com>
2022-12-03 22:03:50 -0300

    Adding responsiveness

--- src/components/layout/Banner.js
+++ src/components/layout/Banner.js
@@ -2,7 +2,7 @@ import developer from '../../img/developer.svg'
 
 function Banner() {
     return (
-        <section className="opc p-7 sm:p-12 lg:flex items-center">
+        <section className="w-[90vw] md:w-[60vw] mx-auto p-7 sm:p-16 lg:w-auto lg:mx-0 lg:flex items-center">
             <div>
                 <h1 className="text-center font-Silkscreen font-bold text-light_text dark:text-white text-xl sm:text-2xl">Come join our team!</h1>
                 <div className="flex justify-center mt-8">
