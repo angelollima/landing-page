fec28ac3c6d98e7960a9884fc024eaf797886eab
angelollima <angeloantonio442@gmail.com>
2022-12-03 22:09:19 -0300

    Deleting label/input, Adding ternary operation

--- src/components/layout/IconTheme.js
+++ src/components/layout/IconTheme.js
@@ -5,18 +5,15 @@ import { useTheme } from "../../provider/SetTheme"
 
 function IconTheme() {
 
-    const { theme, setTheme } = useTheme();
+    const { theme, setTheme } = useTheme()
 
     return (
-        <button onclick="setTheme()">
-            <label htmlFor="toggle">
-                <input type="checkbox" name="" id="toggle" className="hidden"/>
-                    {theme === "dark" ? (
-                        <HiSun className="bg-stone-400 p-2 rounded-lg text-yellow-400 hover:text-yellow-500 duration-500 cursor-pointer" onClick={() => setTheme("light")} size={40} />
-                        ) : ( 
-                        <HiMoon className="bg-blue-300 p-2 rounded-lg text-white hover:text-zinc-300 duration-500 cursor-pointer" onClick={() => setTheme("dark")} size={40} />
-                    )}
-            </label>
+        <button>
+            {theme === "dark" ? (
+                <HiSun className="bg-stone-400 p-2 rounded-lg text-yellow-400 hover:text-yellow-500 duration-500 cursor-pointer" onClick={() => setTheme("light")} size={40} />
+            ) : ( 
+                <HiMoon className="bg-blue-300 p-2 rounded-lg text-white hover:text-zinc-300 duration-500 cursor-pointer" onClick={() => setTheme("dark")} size={40} />
+            )}
         </button>
     )
 }
